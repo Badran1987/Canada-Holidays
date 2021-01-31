66444175b48c1553cf2ee278f3edd751e219d639
Badran1987 <52219058+Badran1987@users.noreply.github.com>
2021-01-31 18:54:53 -0500

    get history API view

--- js/main.js
+++ js/main.js
@@ -1,47 +1,78 @@
 document.addEventListener("DOMContentLoaded", function () {
-const holidayEl= document.getElementById("holyday");
+// const holidayEl[]= 
 const date= document.getElementById("date");
 const observedDate= document.getElementById("obdate");
 const federal= document.getElementById("fediral");
 const provincesele = document.getElementById("provincesele");
 
 
+function creatHolidays(response){
 
+for (let i = 0; i < response.holidays.length; i++) {
+  
+    // console.log(response.holidays.length);
+  const holidays = response.holidays[i];
+  console.log(holidays);
 
-
-
-function creatHoliday(response){
-    // console.log(response.holiday.nameEn);
-    console.log(response);
-   holidayEl.innerHTML= `<h1 class="name">${response.holiday.nameEn}<h1>`;
-   date.innerHTML= `${response.holiday.date}`;
-   observedDate.innerHTML= `<h1 class="name">${response.holiday.observedDate}<h1>`;
-    if (response.holiday.federal===0) {
-        federal.innerHTML= `<h1 class="name">federal<h1>`;
-    } else {
-        federal.innerHTML= `<h1 class="name">Not federal<h1>`;
-    }
-
-    for (let i = 0; i < response.holiday.provinces.length; i++) {
-        const provinces = response.holiday.provinces[i];
-        console.log(provinces.nameEn);
-        provincesele.innerHTML=`${provinces.nameEn}`
-    }
-
-    // for (let provencesItems = 0; provencesItems < provinces.length; provencesItems++) {
-    //     const element = provinces[provencesItems];
-    //     console.log(element)
-    // }
-    // provinces.innerHTML=`<h1 class="name">${response.holiday.provinces.nameEn}<h1>`
-    // const provinces = response.holiday.provinces[0];
-    // console.log(provinces);
-    
+if (response.holidays[i].federal===0) {
+    document.getElementById("fediral").innerHTML += `federal`
+    document.getElementById("contaner").innerHTML += `<div class="left">
+    <h1 id="holydayName"> ${response.holidays[i].nameEn} </h1>
+    <p class="provincesName"> Ontario ..</p>
+    <p class="holydayDate">  ${response.holidays[i].date} </p>
+    <p class="obsDate">${response.holidays[i].observedDate}</p>
+    <p class="fediral" id="fediral"> Fediral  </p>
+  
+  </div>`
+} else {
+    document.getElementById("contaner").innerHTML += `<div class="left">
+    <h1 id="holydayName"> ${response.holidays[i].nameEn} </h1>
+    <p class="provincesName"> Ontario ..</p>
+    <p class="holydayDate">  ${response.holidays[i].date} </p>
+    <p class="obsDate">${response.holidays[i].observedDate}</p>
+    <p class="fediral" id="fediral">  Not Fediral</p>
+  
+  </div>`
+   
 }
 
 
-function holiday(){
+  
+    
 
-    fetch("https://canada-holidays.p.rapidapi.com/api/v1/holidays/2?year=2021", {
+
+}
+
+}
+
+
+
+
+
+// function holiday(){
+
+//     fetch("https://canada-holidays.p.rapidapi.com/api/v1/holidays/2?year=2021", {
+//         "method": "GET",
+//         "headers": {
+//             "x-rapidapi-key": "6601598254mshe2206a0f002367bp10fd4bjsn2c18bcef824c",
+//             "x-rapidapi-host": "canada-holidays.p.rapidapi.com"
+//         }
+//     })
+//     .then((response)=> response.json())
+//     .then(response => {
+//         creatHolidays(response)
+//         // console.log(response);
+//     })
+//     .catch(err => {
+//         console.error(err);
+//     });
+   
+// }
+
+
+function getHolidays(){
+
+    fetch("https://canada-holidays.p.rapidapi.com/api/v1/holidays?year=2021&federal=1", {
         "method": "GET",
         "headers": {
             "x-rapidapi-key": "6601598254mshe2206a0f002367bp10fd4bjsn2c18bcef824c",
@@ -50,13 +81,14 @@ function holiday(){
     })
     .then((response)=> response.json())
     .then(response => {
-        creatHoliday(response)
+        creatHolidays(response);
         // console.log(response);
     })
     .catch(err => {
         console.error(err);
     });
-   
 }
-holiday();
+// holiday();
+getHolidays();
 });
+
